aff16c66d260818673a4b845291fd62f9f184928
cafreeman <cfreeman.code@gmail.com>
2015-12-29 17:52:12 -0600

    Remove commented code
    Duh

--- app/routes/package-list/pkg-info.js
+++ app/routes/package-list/pkg-info.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     let pkgDetailURL = `http://crandb.r-pkg.org/${params.pkgName}`;
-    // return params.pkgName;
     return $.getJSON(pkgDetailURL);
   }
 });
