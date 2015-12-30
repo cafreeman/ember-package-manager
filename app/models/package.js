8e979c4dbad6ea0c1d74b059c7d271a7fd4c50f0
cafreeman <cfreeman.code@gmail.com>
2015-12-30 15:13:00 -0600

    Define `package` model for new adapter

---
+++ app/models/package.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  version: DS.attr('string'),
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+  publicationDate: DS.attr('date'),
+  depends: DS.attr(),
+  imports: DS.attr()
+});
