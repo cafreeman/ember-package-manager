b66ac2d8306793ef9343b931836addb164eb89e4
cafreeman <cfreeman.code@gmail.com>
2015-12-30 15:20:02 -0600

    Remove old `package-list` related files

--- app/routes/package-list/pkg-info.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(params) {
-    let pkgDetailURL = `http://crandb.r-pkg.org/${params.pkgName}`;
-    return $.getJSON(pkgDetailURL);
-  }
-});
