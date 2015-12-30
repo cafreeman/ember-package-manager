45bb3791b668ab412e6d6b102da7b3b1374bcc36
cafreeman <cfreeman.code@gmail.com>
2015-12-30 16:42:31 -0600

    `pkg-info` route now pulls data from `DS.Store`
    Since the package list is now JSONAPI compliant, it became a lot easier to store relevant details in the json file itself. As a result, there's no longer a need to look up package info from the cranDB API.
    
    Other bonus is that since `pkg-info` is nested under `packages` it can pull all the model info using `peekRecord` and remove any extra network traffic

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('packages', function() {
     this.route('pkg-info', {
-      path: ':pkgName'
+      path: ':id'
     });
   });
 });
