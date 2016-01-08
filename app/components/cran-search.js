6231c399a7d01c700f6d5217d01ed2b264ed9b11
cafreeman <cfreeman.code@gmail.com>
2016-01-08 12:13:38 -0600

    Implement `cran-search` component
    Using the `seer.r-pkg.org` API along `ember-power-select` to build a type-ahead search box that lets us search through CRAN packages.
    
    This will be used to provide an intuitive interface for finding and adding packages to our build.

--- app/components/cran-search.js
+++ app/components/cran-search.js
@@ -1,10 +1,48 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // currentPkg is the `selected` property for `power-select`
   currentPkg: '',
 
+  // _performCRANSearch is a component method that queries the cranDB elastic search client
+  // This method is not meant to be called explicitly, but is instead targeted by the `searchCRAN`
+  // action. This allows us to debounce the search action in `power-select` and (hopefully) not
+  // overload the API.
+  // @param query is what the user is typing into the search box
+  _performCRANSearch(query, resolve, reject) {
+    if (Ember.isBlank(query)) {
+      return resolve([]);
+    }
+    // use the query string as a query param on the seer elasticSearch API.
+    // NOTE: there is a `*` at the very end of the API's query param so that we get something that
+    // looks like typeahead search by package name. There may be a better way to do this with
+    // elasticSearch, but this seems to working fine so far.
+    const url = `http://seer.r-pkg.org:9200/_search?q=_id:${query}*`;
+    return $.getJSON(url)
+    .then((json) => {
+      // pull out the `hits` array of search matches and map over them to return a more compact
+      // object for each result.
+      let results = json.hits.hits;
+      resolve(results.map((pkg) => {
+        return {
+          name: pkg._id,
+          description: pkg._source.Description
+        };
+      }));
+    }, (err) => {
+      reject(err);
+    });
+  },
   actions: {
+    // searchCRAN is the `search` action called from `power-select` and wraps the `debounce` method
+    // in a RSVP.Promise so we can prevent an ajax request from being trigged on every keypress
+    searchCRAN(query) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.run.debounce(this, this._performCRANSearch, query, resolve, reject, 500);
+      });
     },
+    // update is the `onchange` action called from `power-select` and simply puts the selected
+    // search result in the top level of the search box
     update(selected) {
       this.set('currentPkg', selected);
     }
