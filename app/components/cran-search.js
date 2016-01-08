e840c4b228b3216b53fd4452bc1d16701dc89df2
cafreeman <cfreeman.code@gmail.com>
2016-01-08 12:12:13 -0600

    Removing cruft from the cran search experiment

--- app/components/cran-search.js
+++ app/components/cran-search.js
@@ -1,26 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // packages: ['dplyr', 'magrittr'],
-  // packages() {
-  //   let packageListURL = 'http://crandb.r-pkg.org/-/desc?limit=10';
-  //   return $.getJSON(packageListURL)
-  //     .then((packages) => {
-  //       return Object.keys(packages);
-  //     });
-  // },
-  // packageList: Ember.computed('packageList.[]', function() {
-  //   return this.get('packageList')
-  // })
   currentPkg: '',
 
   actions: {
-    searchCRAN() {
-      let packageListURL = 'http://crandb.r-pkg.org/-/desc?limit=500';
-      return $.getJSON(packageListURL)
-        .then((packages) => {
-          return Object.keys(packages);
-        });
     },
     update(selected) {
       this.set('currentPkg', selected);
--- app/routes/package-search.js
+++ app/routes/package-search.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   return $.getJSON('http://crandb.r-pkg.org/-/desc')
-  //     .then(packages => Object.keys(packages));
-  // }
 });
