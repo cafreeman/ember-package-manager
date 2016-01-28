dd065874ab63937eead196bc6495473f7c3ea8d2
Chris Freeman <cfreeman.code@gmail.com>
2016-01-28 17:56:50 -0600

    `index` route redirects based on auth status
    `beforeModel` hook checks the session for an authenticated user and either routes to the main (`packages`) route or to the `login` route.
    
    `index` is used since it is loaded immediately following the `application` route, where we fetch the existing session

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('packages');
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('packages');
+    } else {
+      this.transitionTo('login');
+    }
   }
 });
