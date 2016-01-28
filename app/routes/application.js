d3452e143a4a84f331aab23887e8b38894470e85
Chris Freeman <cfreeman.code@gmail.com>
2016-01-28 17:54:53 -0600

    Fetch existing sessions in the `application` route
    Use the `beforeModel` hook to call EmberFire's `session.fetch()` method to grab an existing session (if it exists).
    
    Fetching the existing session allows the app to recognize authenticated users even after page reloads.
    
    We `return` the `fetch` method in order to force a synchronous fetch. This is useful because it makes the existing session available further down the route chain, allowing us to check if the user is authenticated and redirect accordingly.

---
+++ app/routes/application.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel() {
+    // Retrieve the current user's session (if it exists)
+    return this.get('session').fetch().catch(error => console.log(error));
+  }
+});
