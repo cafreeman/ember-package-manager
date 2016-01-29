8c18ffe016c8731435d0317f4d771d1ecaa1140a
Chris Freeman <cfreeman.code@gmail.com>
2016-01-28 18:30:43 -0600

    Added a shell for login/signup action on the route
    The `auth-form` component now has a `on-submit` argument that you can call when instantiating it.
    
    Inside the component, the `login` and `signUp` actions will trigger the `on-submit` action with `Component.sendAction`.
    
    In the `login` template, the `on-submit` action is mapped to the routes's `loginOrSignUp` action, which accepts an `isLogin` boolean and will dispatch a login or signUp method accordingly.

--- app/components/auth-form.js
+++ app/components/auth-form.js
@@ -10,9 +10,11 @@ export default Ember.Component.extend({
     },
     login() {
       console.log('logging in!');
+      this.sendAction('on-submit', true);
     },
     signUp() {
       console.log('signing up!')
-    }
+      this.sendAction('on-submit', false);
+    },
   }
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    loginOrSignUp(isLogin) {
+      if (isLogin) {
+        console.log("triggering login from the route");
+      } else {
+        console.log("triggering signup from the route");
+      }
+    }
+  }
 });
