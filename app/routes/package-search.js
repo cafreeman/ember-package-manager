7392858c1d1e45dfdb82d75aaeda2f7cb3dabcad
Chris Freeman <cfreeman.code@gmail.com>
2016-01-29 11:22:45 -0600

    Add redirects for non-authenticated users
    `add-package` and `package-search` routes now redirect to `packages` if the user is not authenticated.
    
    The nav links are already disabled for these routes if the user is not logged in, however, the redirection is an added layer of security since the user could theoretically manually type the other URLs in.

--- app/routes/package-search.js
+++ app/routes/package-search.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   cart: Ember.inject.service('package-cart'),
 
+  beforeModel() {
+    if (!this.get('session.isAuthenticated')) {
+      this.transitionTo('login');
+    }
+  },
+
   model() {
     return this.get('cart.packagesToAdd');
   },
