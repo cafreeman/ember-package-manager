632da3c80188ef9f1ec78f950e50988d8ff3a8c3
Chris Freeman <cfreeman.code@gmail.com>
2016-01-28 18:45:15 -0600

    Basic shape of auth-based navbar
    Starting to mess with dynamically rendering parts of the navbar based on auth status.
    
    Also added a `logout` button and action, although right now all it does is print to the console.

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,10 @@ export default Ember.Route.extend({
   beforeModel() {
     // Retrieve the current user's session (if it exists)
     return this.get('session').fetch().catch(error => console.log(error));
+  },
+  actions: {
+    logOut() {
+      console.log('logging out!');
+    }
   }
 });
