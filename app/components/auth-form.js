b127b406d210ccbc09bdf3e1420779a760823889
Chris Freeman <cfreeman.code@gmail.com>
2016-02-01 15:04:52 -0600

    Add form validation using `ember-validations`
    The `auth-form` component now has built-in form validations for each field using the `ember-validations` addon combined with built-in validation functionality from `ember-bootstrap`'s form components.
    
    The top-level `auth-form` component extends `EmberValidations` and implements the form properties and validations inside the component.
    
    These properties are then passed into the `model` property of the `bs-form` components, which automatically triggers the bootstrap validation markup and subsequent behaviors.
    
    One of the added features is that the `bs-form` component automatically stifles the `submit` action if any of the validations are failing, so we don't have to worry about preventing faulty form submissions to the server.

--- app/components/auth-form.js
+++ app/components/auth-form.js
@@ -1,7 +1,54 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Component.extend({
+// export default Ember.Component.extend({
+export default Ember.Component.extend(EmberValidations, {
   signUpForm: false,
+
+  firstName: null,
+  lastName: null,
+  email: null,
+  password: null,
+  passwordConfirmation: null,
+
+  validations: {
+    firstName: {
+      presence: {
+        if: 'signUpForm',
+        message: 'You must provide your first name.'
+      }
+    },
+    lastName: {
+      presence: {
+        'if': 'signUpForm',
+        message: 'You must provide your last name.'
+      }
+    },
+    email: {
+      presence: {
+        message: 'You must provide an email address.'
+      },
+      format: {
+        with: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+        message: 'The email you provided is invalid.'
+      },
+    },
+    password: {
+      length: {
+        minimum: 6,
+        message: 'Your password must be at least 6 characters.'
+      },
+      confirmation: {
+        'if': 'signUpForm',
+        message: 'Passwords must match.'
+      }
+    },
+    passwordConfirmation: {
+      presence: {
+        'if': 'signUpForm'
+      }
+    }
+  },
 
   actions: {
     toggleSignUpForm() {
