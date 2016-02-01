2aa2999b7e9ba90d38c378075d913e79f03e7664
Chris Freeman <cfreeman.code@gmail.com>
2016-02-01 13:38:30 -0600

    Refactor the login and signup to controller
    Moved all of the auth functions to a `login` controller instead of being on the route. Having the functionality on the route was making it difficult to have the UI react to failed logins, so now the controller lets us track login state and reflect failed logins with an alert box.

---
+++ app/controllers/login.js
@@ -0,0 +1,67 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  firebase: Ember.inject.service(),
+
+  isInvalidLogin: false,
+
+  // authenticate against the firebase service
+  login(userInfo) {
+    let {email, password} = userInfo;
+    this.get('session').open('firebase', {
+      provider: 'password',
+      email,
+      password
+    }).
+      then((success) => {
+        console.log(success);
+        this.transitionToRoute('packages');
+      }, (err) => {
+        this.toggleProperty('isInvalidLogin');
+        console.log(this.get('isInvalidLogin'));
+      });
+  },
+
+  // sign up as a new user with the Firebase service. This not only registers a user, but also
+  // creates a new User record in the firebase collection so that we can have persistent user info
+  signUp(userInfo) {
+    let {firstName, lastName, email, password} = userInfo;
+
+    this.get('firebase').createUser({
+      email,
+      password
+    }, (err, userData) => {
+      if (err) {
+        alert(err);
+      } else {
+        this.get('session').open('firebase', {
+          provider: 'password',
+          email,
+          password
+        })
+          .then(() => {
+            let user = this.store.createRecord('user', {
+              id: userData.uid,
+              firstName,
+              lastName,
+              email
+            });
+            user.save().then(() => this.transitionToRoute('packages'));
+          });
+      }
+    });
+  },
+
+
+  actions: {
+    // dispatch the login or signUp method based on the (boolean) value of `isLogin`, which is sent
+    // from the `auth-form` component
+    loginOrSignUp(isLogin, userInfo) {
+      if (isLogin) {
+        this.login(userInfo);
+      } else {
+        this.signUp(userInfo);
+      }
+    }
+  }
+});
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,67 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  firebase: Ember.inject.service(),
-
   // redirect to the main page if the user is already logged in
   beforeModel() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('packages');
-    }
-  },
-
-  // authenticate against the firebase service
-  login(userInfo) {
-    let {email, password} = userInfo;
-    this.get('session').open('firebase', {
-      provider: 'password',
-      email,
-      password
-    }).
-    then(() => {
-      this.transitionTo('packages');
-    });
-  },
-
-  // sign up as a new user with the Firebase service. This not only registers a user, but also
-  // creates a new User record in the firebase collection so that we can have persistent user info
-  signUp(userInfo) {
-    let {firstName, lastName, email, password} = userInfo;
-
-    this.get('firebase').createUser({
-      email,
-      password
-    }, (err, userData) => {
-      if (err) {
-        alert(err);
-      } else {
-        this.get('session').open('firebase', {
-          provider: 'password',
-          email,
-          password
-        })
-          .then(() => {
-            let user = this.store.createRecord('user', {
-              id: userData.uid,
-              firstName,
-              lastName,
-              email
-            });
-            user.save().then(() => this.transitionTo('packages'));
-          });
-      }
-    });
-  },
-
-  actions: {
-    // dispatch the login or signUp method based on the (boolean) value of `isLogin`, which is sent
-    // from the `auth-form` component
-    loginOrSignUp(isLogin, userInfo) {
-      if (isLogin) {
-        this.login(userInfo);
-      } else {
-        this.signUp(userInfo);
-      }
     }
   }
 });
